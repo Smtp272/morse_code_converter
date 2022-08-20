eceb3331bbfb90acaaf32b226987e521ed438962
Smtp272 <101472734+Smtp272@users.noreply.github.com>
2022-08-20 11:07:47 +0300

    Add customtkinter,rules label. Make exception_manager a staticmethod, modify array joining to use space

--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
 import json
 import tkinter
-from tkinter import Label, Button,END,Text
+from tkinter import Label, Button, END, Text
+import customtkinter
+from customtkinter import CTkButton
 
 ###CONSTS
 headings = ("Montserrat", 15)
@@ -15,7 +17,7 @@ class MorseCodeConverter:
         super().__init__()
 
         ###ROOT
-        self.root = tkinter.Tk()
+        self.root = customtkinter.CTk()
         self.root.title("Morse code")
         self.root.geometry("900x550")
         self.root.eval('tk::PlaceWindow . center')
@@ -35,33 +37,42 @@ class MorseCodeConverter:
         self.translated_textbox.grid(column=2, row=2)
 
         ###BUTTONS
-        self.translate_button = Button(self.root, text="Translate !", font=translate_btn, command=self._translate_text)
+        self.translate_button = CTkButton(self.root, text="Translate !", text_font=translate_btn,
+                                          command=self._translate_text)
         self.translate_button.grid(column=1, row=2, padx=20)
 
-        self.clear_button = Button(self.root, text="Clear", command=self._clear_textboxes)
-        self.clear_button.grid(row=3, column=1, ipadx=20)
+        self.clear_button = CTkButton(self.root, text="Clear", command=self._clear_textboxes)
+        self.clear_button.grid(row=3, column=1, ipadx=5)
+
+        ##RULES
+        self.rules_label = Label(self.root,
+                                 text="# - Untranslatable character\n/ - Word separator\nSpace - Letter separator",
+                                 bg=bg_color, fg="red")
+        self.rules_label.grid(row=4, column=1)
 
         ###MAINLOOP
         self.root.mainloop()
+
     def _translate_text(self):
         self.translated_textbox.delete(1.0, END)
         words_to_translate = self.original_textbox.get('1.0', 'end')[:-1]
         words_to_translate = words_to_translate.lower()
         with open("code.json", "rb") as file:
             data = json.load(file)['text']
-            def exception_manager(a):
-                try:
-                    return data[a]
-                except KeyError:
-                    pass
-            arr = [exception_manager(i) for i in words_to_translate]
-        self.translated_textbox.insert(1.0, "".join(arr))
+            arr = [MorseCodeConverter.exception_manager(data, i) for i in words_to_translate]
+        self.translated_textbox.insert(1.0, " ".join(arr))
 
+    @staticmethod
+    def exception_manager(data, a):
+        try:
+            return data[a]
+        except KeyError:
+            return "#"
 
     def _clear_textboxes(self):
         self.translated_textbox.delete(1.0, END)
         self.original_textbox.delete(1.0, END)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     t = MorseCodeConverter()
